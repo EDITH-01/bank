f51106c8090a7b27ddb274aeed3d550685faa007
EDITH-01 <rubenomega1809@gmail.com>
2024-11-04 14:34:24 +0530

    40:10 approx

---
+++ components/TotalBalanceBox.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const TotalBalanceBox = ({
+    accounts =[],totalBanks,totalCurrentBalance
+}: TotalBalanceBoxProps ) =>{
+
+  return (
+   <section className='total-balance'>
+    <div className='total-balance-chart'>
+
+    </div>
+    <div className='flex flex-col gap 2'>
+
+    </div>
+
+   </section>
+  )
+}
+
+export default TotalBalanceBox
